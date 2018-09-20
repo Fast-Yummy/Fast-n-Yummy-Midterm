edb77cc86403ae211b2ebc0213b4b950ee7dd45c
Suganthi Jayaseelan <suganthi1483@gmail.com>
2018-09-20 19:51:21 +0000

    Routes

--- routes/users.js
+++ routes/users.js
@@ -2,17 +2,43 @@
 
 const express = require('express');
 const router  = express.Router();
+const async   = require('async');
+const twilio  = require('../server/twilio');
+
+//Add to Cart
+const addtocart = (foodid)=>{
+cart = [];
+cart.push(foodid);
+}
+// Remove from Cart
+const removefromcart = (foodid) =>{
+cart = [];
+cart.remove(foodid);
+
+
+
 
 module.exports = (knex) => {
 
-  router.get("/", (req, res) => {
-    knex
-      .select("*")
-      .from("users")
-      .then((results) => {
-        res.json(results);
-    });
+router.get("/", (req, res) => {
+  knex
+   .select("*")
+   .from("users")
+   .then((results) => {
+    res.json(results);
   });
-
-  return router;
+ });
+​
+ return router;
 }
+
+/*const calculateTotal = (order) => { // Need to work 
+  let total = 0;
+  let quantity;
+  order.foodid.forEach((foodid) => {
+    let quantity
+    total += (fooditem.price * quantity)
+  })
+ 
+  return total * 1.13;
+}*/
